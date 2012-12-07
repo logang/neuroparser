8a1d1d23ba032f69de87cdd52f10690ef757b6c2
Logan Grosenick <logang@gmail.com>
2012-12-07 01:12:49 -0800

    Cleaned up examples. Need to add more

--- examples/graphnet_example.py
+++ examples/graphnet_example.py
@@ -4,6 +4,9 @@ from optimization.cwpath import cwpath, graphnet
 from optimization.cwpath.mask import prepare_adj
 import scipy.optimize
 
+#--------------------------------------------------------------------------
+# A simple use case on fake data
+
 n = 25
 #Generate toy data: 3x3x3x3 data on n trials
 X_orig = np.random.normal(0,1,n*3**4).reshape((n,3,3,3,3))
@@ -33,19 +36,8 @@ for i in range(n):
     beta =np.round(1000*l.problem.coefficients)/1000
     print beta
 
-
-    """
-    def huber(r):
-            r = np.fabs(r)
-                t = np.greater(r, delta)
-                    return (1-t)*r**2 + t*(2*delta*r - delta**2)
-                def f(b):
-                        return huber(Y - np.dot(X, b)).sum()/2  + np.fabs(b).sum()*l1 + l2 * np.linalg.norm(b)**2/2
-                    v = scipy.optimize.fmin_powell(f, np.zeros(X.shape[1]), ftol=1.0e-10, xtol=1.0e-10, maxfun=1e6)
-                    v = np.round(1000*np.asarray(v))/1000
-                    print beta
-                    print v
-                    """
+#--------------------------------------------------------------------------
+# Test the Naive GraphNet classifier
 
 def test_NaiveGraphNet_classification( n, p, num_obs_class1 = 100, num_obs_class2 = 100 ):
     """
